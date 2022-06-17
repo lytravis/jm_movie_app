c33503da5a6cd53ee3740e0738090a32094b9afe
Travis <travisbly@gmail.com>
2022-06-17 00:16:06 -0700

    added api to app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 //f749e32c
-
+const API_URL = 'http://www.omdbapi.com?apikey=f749e32c';
+// const API_URL = 'http://www.omdbapi.com?apikey=b6003d8a';
+// const API_URL = 'https://www.omdbapi.com?apikey=f749e32c&';
 
 function App() {
-  return (
-    <div>App</div>
-  )
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+
+    console.log(data.Search);
+  };
+
+  useEffect(() => {
+    searchMovies('Superman');
+  }, []);
+
+  return <div>App</div>;
 }
 
-export default App
+export default App;
